c2e14ea5f4704b957c8055417bf1faf31c9da117
jisung <jisung6723@gmail.com>
2025-06-29 13:00:41 +0900

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,35 @@
 // 2024.06.26
 
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Coconut");
-    window.setFramerateLimit(144);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
-        window.clear();
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
         window.display();
     }
 }
+
